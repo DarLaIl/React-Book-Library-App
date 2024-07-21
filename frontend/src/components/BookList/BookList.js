947b1121c37414b3f48a80666fb8a077f3e36c1f
DarLaIl <darlail.work.mail@gmail.com>
2024-07-21 22:50:29 +0300

    Получение данных из Redux store с помощью хука useSelector

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,9 +1,24 @@
+import { useSelector } from 'react-redux';
 import './BookList.css';
 
 const BookList = () => {
+  const books = useSelector((state) => state.books);
   return (
     <div className='app-block book-list'>
       <h2>Book List</h2>
+      {books.lenght === 0 ? (
+        <p>No books avaliable</p>
+      ) : (
+        <ul>
+          {books.map((book, i) => (
+            <li key={i}>
+              <div className='book-info'>
+                {++i}. {book.title} by <strong>{book.autor}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
