35987a4d1acdd7d30dba9593eb400553d5c72820
DarLaIl <darlail.work.mail@gmail.com>
2024-07-21 23:27:32 +0300

    Генерация уникальных id для книг

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import './BookList.css';
 
 const BookList = () => {
-    const books = useSelector((state) => state.books);
+  const books = useSelector((state) => state.books);
   return (
     <div className='app-block book-list'>
       <h2>Book List</h2>
@@ -11,9 +11,9 @@ const BookList = () => {
       ) : (
         <ul>
           {books.map((book, i) => (
-            <li key={i}>
+            <li key={book.id}>
               <div className='book-info'>
-                      {++i}. {book.title} by {' '}<strong>{book.autor}</strong>
+                {++i}. {book.title} by <strong>{book.autor}</strong>
               </div>
             </li>
           ))}
