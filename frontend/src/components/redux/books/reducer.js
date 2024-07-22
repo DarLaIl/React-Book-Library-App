5c8aae08aef3ae6830e341e1cd67fc6c7e081fdb
DarLaIl <darlail.work.mail@gmail.com>
2024-07-22 21:18:05 +0300

    Удаление книги из списка по id

--- frontend/src/components/redux/books/reducer.js
+++ frontend/src/components/redux/books/reducer.js
@@ -1,16 +1,16 @@
 import * as actionTypes from './actionTypes';
 
-
-const initialState = []
+const initialState = [];
 
 const booksReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case actionTypes.ADD_BOOK:
-            return [...state, action.payload]
-   
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case actionTypes.ADD_BOOK:
+      return [...state, action.payload];
+    case actionTypes.DELETE_BOOK:
+      return state.filter((book) => book.id !== action.payload);
+    default:
+      return state;
+  }
+};
 
-export default booksReducer
+export default booksReducer;
